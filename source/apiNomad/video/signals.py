20218668009fdc1e5ccbf5787dfdd96e9a6c3392
willy <willkoua@gmail.com>
2019-05-28 19:01:39 -0400

    add signal for delete image after delete video

--- source/apiNomad/video/signals.py
+++ source/apiNomad/video/signals.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.db.models.signals import pre_delete
+from django.db.models.signals import pre_delete, post_delete
 from django.dispatch import receiver
 
 from .models import Video, Image
@@ -16,7 +16,31 @@ def signal_file_delete_before_delete_video(sender, instance, **kwargs):
     :param kwargs:
     :return:
     """
-    deleteEmptyRepository(settings.MEDIA_ROOT + '/' + instance.file.name)
+    try:
+        print('je suis dans le signal de video')
+        deleteEmptyRepository(settings.MEDIA_ROOT + '/' + instance.file.name)
+    except FileNotFoundError as e:
+        print('je suis dans le signal ex filenot')
+        pass
+
+
+@receiver(post_delete, sender=Video)
+def signal_image_delete_after_delete_video(sender, instance, **kwargs):
+    """
+    deletes the video file before its data is deleted from the database
+
+    :param sender: reference model on which the function should be called
+    :param instance: data deleted
+    :param kwargs:
+    :return:
+    """
+    try:
+        print('je suis dans le signal de video apres delete')
+        if instance.avatar:
+            instance.avatar.delete()
+    except FileNotFoundError as e:
+        print('je suis dans le signal ex filenot')
+        pass
 
 
 @receiver(pre_delete, sender=Image)
@@ -29,4 +53,9 @@ def signal_file_delete_before_delete_image(sender, instance, **kwargs):
     :param kwargs:
     :return:
     """
-    deleteEmptyRepository(settings.MEDIA_ROOT + '/' + instance.file.name)
+    try:
+        print('je suis dans le signal de image')
+        deleteEmptyRepository(settings.MEDIA_ROOT + '/' + instance.file.name)
+    except FileNotFoundError as e:
+        print('je suis dans le signal de image exp filenot')
+        pass
